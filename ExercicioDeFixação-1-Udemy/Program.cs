f22962386bc2a1e3681bde60ed5d9f6200582075
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2025-02-13 16:46:46 -0300

    Att

--- ExercicioDeFixação-1-Udemy/Program.cs
+++ ExercicioDeFixação-1-Udemy/Program.cs
@@ -6,6 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            //Ex1
             string Produto1 = "Computador";
             string Produto2 = "Mesa de escritório";
             int idade = 30;
@@ -21,6 +22,25 @@ namespace MyApp
             Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
             Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
             Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", System.Globalization.CultureInfo.InvariantCulture));
+            //Ex2
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultimoNome = vet[0];
+            int idade2 = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2]);
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine($"{ preco:F2}");
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade2);
+            Console.WriteLine(altura);
+
         }
     }
 }
